ec6fe4ffb70fda76f219f471d2bfcfa5255fb782
Trevor_Umeda <trevor.umeda@gmail.com>
2011-08-08 05:41:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsuranceApp::Application.config.secret_token = 'e89d611ab9963b57dffec6815f8bc7e432a68a92fa7ffe4896b6bbd8158bd65bd440f397debb2c37efc75a403f4d3af7819af236f3760d7afc4879e9e33d88c7'
